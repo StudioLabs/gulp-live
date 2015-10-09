6fbd044e6c6b7f07ad72b46549e4c0c5119b8bdd
Monteiro Steed <steed.monteiro@gmail.com>
2015-10-09 17:27:32 +0200

    update

--- index.js
+++ index.js
@@ -3,29 +3,31 @@ var http = require("http");
 var Live = require("live-edit");
 var util = require("gulp-util");
 
+var live = new Live();
+
 function LiveServer() {
-	this.live = new Live();
+
 }
 
-LiveServer.prototype.server = function(options) {
-	this.live.server(options);
+LiveServer.prototype.connect = function(options) {
+	live.connect(options);
 };
 
 LiveServer.prototype.devtools = function(options) {
-	this.live.devtools(options);
+	live.devtools(options);
 };
 
 LiveServer.prototype.watch = function(options) {
-	this.live.watch(options);
+	live.watch(options);
 };
 
 LiveServer.prototype.start = function(options) {
-	this.live.start(options);
+	live.start(options);
 };
 
 LiveServer.prototype.reload = function() {
-	if (this.live.ws !== undefined) {
-		this.live.ws.broadcast({action:'reload'});
+	if (live.ws !== undefined) {
+		live.ws.broadcast({action:'reload'});
 	}
 };
 
@@ -34,9 +36,9 @@ LiveServer.prototype.edit = function() {
 	if (files[files.length - 1] === 'function') done = files.pop();
 	done = typeof done === 'function' ? done : function() {};
 	files.forEach(function(file) {
-		this.live.onFileChange(file.path);
+		live.onFileChange(file.path);
 		done();
-	}, this);
+	});
 };
 
 var server = new LiveServer();
